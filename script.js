572d9b7113fbb268f08f66e3edb068da2aafc1c8
jhonmichaelcruzyncil-debug <jhonmichaelcruzyncil@gmail.com>
2025-12-18 15:54:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const eventDate = new Date("December 20, 2025 22:00:00").getTime();
+
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = eventDate - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+}, 1000);
